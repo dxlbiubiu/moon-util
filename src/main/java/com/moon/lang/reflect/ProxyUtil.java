596d0fbfb77bf8b4d8bfd4366b2cee4f6ea00d91
张东民 <xua744531854@163.com>
2019-03-27 09:40:19 +0800

    动态代理测试

--- src/main/java/com/moon/lang/reflect/ProxyUtil.java
+++ src/main/java/com/moon/lang/reflect/ProxyUtil.java
@@ -1,5 +1,11 @@
 package com.moon.lang.reflect;
 
+import com.moon.lang.ClassUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.List;
+
 import static com.moon.lang.ThrowUtil.noInstanceError;
 
 /**
@@ -7,9 +13,16 @@ import static com.moon.lang.ThrowUtil.noInstanceError;
  */
 public final class ProxyUtil {
 
+    private final static ClassLoader LOADER = ProxyUtil.class.getClassLoader();
+
     private ProxyUtil() {
         noInstanceError();
     }
 
-
+    public static <T> T newProxyInstance(T obj, InvocationHandler handler){
+        Class type = obj.getClass();
+        List<Class> classes = ClassUtil.getAllInterfaces(type);
+        Class[] interfaces = classes.toArray(new Class[classes.size()]);
+        return (T) Proxy.newProxyInstance(LOADER, interfaces, handler);
+    }
 }
--- src/test/java/com/moon/lang/reflect/ProxyUtilTestTest.java
+++ src/test/java/com/moon/lang/reflect/ProxyUtilTestTest.java
@@ -1,10 +1,14 @@
 package com.moon.lang.reflect;
 
+import com.moon.util.RandomStringUtil;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author benshaoye
@@ -18,14 +22,82 @@ class ProxyUtilTestTest {
 
     static class Student implements Getter {
 
+        private final String str = RandomStringUtil.next();
+
+        public Student() {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>  " + str);
+        }
+
+        public String getStr() {
+            return str;
+        }
+
         @Override
         public Object get() {
-            return null;
+            return str;
+        }
+    }
+
+    public static class StudentInvocationHandler implements InvocationHandler {
+
+        private final Student student;
+
+        public StudentInvocationHandler() {this(new Student());}
+
+        public StudentInvocationHandler(Student student) {this.student = student;}
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("+++++++++++++++++++++++++++++++++++  before");
+            Object invoked = method.invoke(student, args);
+            System.out.println("+++++++++++++++++++++++++++++++++++  after");
+            return invoked;
+        }
+    }
+
+    @Test
+    void testJDKProxy() {
+        Class[] interfaces = {Getter.class};
+        Object o = Proxy.newProxyInstance(getClass().getClassLoader(), interfaces, new StudentInvocationHandler());
+        Getter getter = (Getter) o;
+        Object res = getter.get();
+        System.out.println("===================================  " + res);
+    }
+
+    @Test
+    void testJDKProxy1() {
+        Student student = new Student();
+        System.out.println(student.get());
+        Class[] interfaces = {Getter.class};
+        Object o = Proxy.newProxyInstance(getClass().getClassLoader(),
+            interfaces, new StudentInvocationHandler(student));
+        Getter getter = (Getter) o;
+        Object res = getter.get();
+        System.out.println("===================================  " + res);
+    }
+
+    public static class StudentMethodInterceptor implements MethodInterceptor {
+
+        private final Student student;
+
+        public StudentMethodInterceptor() {this.student = new Student();}
+
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+            System.out.println("-----------------------------------  before");
+            Object invoked = proxy.invoke(student, args);
+            System.out.println("-----------------------------------  after");
+            return invoked;
         }
     }
 
     @Test
     void testFirstMethod() {
-        Student student = new Student();
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Student.class);
+        enhancer.setCallback(new StudentMethodInterceptor());
+        Student student = (Student) enhancer.create();
+        Object res = student.get();
+        System.out.println("===================================  " + res);
     }
 }
